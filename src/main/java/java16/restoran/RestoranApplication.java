20cb1445017c9a5031b34d369705959290b4b9d7
Bkarybaev <baielkarybaev@gmail.com>
2025-03-13 21:16:33 +0600

    Initial commit

---
+++ src/main/java/java16/restoran/RestoranApplication.java
@@ -0,0 +1,13 @@
+package java16.restoran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java16/restoran/RestoranApplicationTests.java
@@ -0,0 +1,13 @@
+package java16.restoran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
